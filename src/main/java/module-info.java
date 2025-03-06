ee476ae2e650f362eb0a9e0767d20c5f6e0c4b8a
radalana <radalana415@gmail.com>
2025-03-06 14:28:07 +0100

    comment: in module-info.java unused dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,12 +2,13 @@ module at.technikum_wien.tourplanner {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires javafx.graphics;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
+    //requires org.controlsfx.controls;
+    //requires com.dlsc.formsfx;
+    //requires net.synedra.validatorfx;
+    //requires org.kordamp.ikonli.javafx;
+    //requires org.kordamp.bootstrapfx.core;
     //requires eu.hansolo.tilesfx;
 
     opens at.technikum_wien.tourplanner to javafx.fxml;
