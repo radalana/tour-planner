88fda30cd4947b1412a2be2108400cf9d5f8ceb6
radalana <radalana415@gmail.com>
2025-03-06 13:07:26 +0100

    rename Controller.java and Main.java

---
+++ src/main/java/at/technikum_wien/tourplanner/Controller.java
@@ -0,0 +1,14 @@
+package at.technikum_wien.tourplanner;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
