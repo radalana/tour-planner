0e33f9ca07c4c85763d0bee69f4bf3d5690fcdb1
radalana <radalana415@gmail.com>
2025-03-10 21:02:34 +0100

    update ControllerFactory: add TourTableController

--- src/main/java/at/technikum_wien/tourplanner/view/ControllerFactory.java
+++ src/main/java/at/technikum_wien/tourplanner/view/ControllerFactory.java
@@ -1,19 +1,28 @@
 package at.technikum_wien.tourplanner.view;
 
 import at.technikum_wien.tourplanner.viewmodel.HomepageViewModel;
+import at.technikum_wien.tourplanner.viewmodel.TourTableViewModel;
 
 public class ControllerFactory {
     private final HomepageViewModel homepageViewModel;
+    private final TourTableViewModel tourTableViewModel;
 
 
     public ControllerFactory() {
         homepageViewModel = new HomepageViewModel();
+        tourTableViewModel = new TourTableViewModel();
     }
 
     //Factory-Method Pattern
     public Object create(Class<?> controllerClass) {
-        //TODO if (controllerClass == HomepageViewModel.class) {
+        if (controllerClass == HomepageController.class) {
             return new HomepageController(homepageViewModel);
+        }else if (controllerClass == TourTableController.class) {
+            return new TourTableController(tourTableViewModel);
+        }else {
+            throw new IllegalArgumentException("Unknown controller class: " + controllerClass);
+        }
+
     }
 
     // Singleton-Pattern with early-binding
