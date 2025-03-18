79ef5d1b557018545fdd58eaddd069bcdcab75c1
radalana <radalana415@gmail.com>
2025-03-18 19:18:29 +0100

    update HeaderController.java fixed the search bar, fixed the filter dropdown menu

--- src/main/java/at/technikum_wien/tourplanner/view/HeaderController.java
+++ src/main/java/at/technikum_wien/tourplanner/view/HeaderController.java
@@ -1,4 +1,38 @@
 package at.technikum_wien.tourplanner.view;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class HeaderController {
+    @FXML
+    public TextField searchField;
+    @FXML
+    private AnchorPane headerPane;
+    @FXML
+    public MenuButton filterMenu;
+    @FXML
+    public CheckBox popularityCheckBox;
+    @FXML
+    public CheckBox childFriendlinessCheckBox;
+
+    @FXML
+    public void initialize() {
+        //initial focus is set away from searchfield
+        Platform.runLater(() -> headerPane.requestFocus());
+
+        setupCheckBox(popularityCheckBox);
+        setupCheckBox(childFriendlinessCheckBox);
+    }
+
+    private void setupCheckBox(CheckBox checkBox) {
+        checkBox.setOnAction(event -> {
+            event.consume();
+            filterMenu.show();
+        });
+    }
 }
