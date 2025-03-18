4844c1978e09ba291a6d48865cce01c327aaef97
radalana <radalana415@gmail.com>
2025-03-18 10:51:30 +0100

    update: new tourDetails.fxml

--- src/main/java/at/technikum_wien/tourplanner/viewmodel/TourTableViewModel.java
+++ src/main/java/at/technikum_wien/tourplanner/viewmodel/TourTableViewModel.java
@@ -1,4 +1,12 @@
 package at.technikum_wien.tourplanner.viewmodel;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class TourTableViewModel {
+    private final ObservableList<HighscoreEntry> data =
+            FXCollections.observableArrayList(
+                    new HighscoreEntry("daniel", "infinite"),
+                    new HighscoreEntry("not daniel", "few")
+            );
 }
