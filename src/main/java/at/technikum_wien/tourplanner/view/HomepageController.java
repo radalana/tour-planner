51a8e7f9cace0c1c19f6db5c07efe2b4e52f90e4
radalana <radalana415@gmail.com>
2025-03-25 14:07:28 +0100

    update HomepageController.java delete loadTourDetailsView() - now in TourTable

--- src/main/java/at/technikum_wien/tourplanner/view/HomepageController.java
+++ src/main/java/at/technikum_wien/tourplanner/view/HomepageController.java
@@ -1,14 +1,9 @@
 package at.technikum_wien.tourplanner.view;
 
-import at.technikum_wien.tourplanner.AppEventAggregator;
-import at.technikum_wien.tourplanner.FXMLDependencyInjection;
 import at.technikum_wien.tourplanner.NewTourCloseListener;
-import at.technikum_wien.tourplanner.events.TourSelectedEvent;
 import at.technikum_wien.tourplanner.viewmodel.HomepageViewModel;
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
-import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
 
 public class HomepageController {
@@ -35,11 +30,6 @@ public class HomepageController {
                 overlayPane.setMouseTransparent(true);
             }
         });
-        /*
-        homepageViewModel.getAppEventAggregator().subscribe(TourSelectedEvent.class, event -> {
-            loadTourDetailsView();
-        })
-         */
     }
 
     @FXML private void showPopupNewTour(ActionEvent actionEvent) {
@@ -48,17 +38,4 @@ public class HomepageController {
         //TODO not clickable wenn popup
         //TODO darker
     }
-
-    private void loadTourDetailsView() {
-        try {
-            Parent tourDetailsView = FXMLDependencyInjection.load("tourDetails.fxml");
-            Platform.runLater(() -> {
-                rootLayout.getChildren().setAll(tourDetailsView);
-            });
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
 }
