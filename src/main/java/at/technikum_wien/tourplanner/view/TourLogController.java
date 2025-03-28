0ef736c13cda4fc29d7032a2ccc18761e960e8c0
radalana <radalana415@gmail.com>
2025-03-28 17:59:03 +0100

    update TourLogController.java: right mouse click -> menu edit/delete, not functional!!!

--- src/main/java/at/technikum_wien/tourplanner/view/TourLogController.java
+++ src/main/java/at/technikum_wien/tourplanner/view/TourLogController.java
@@ -5,9 +5,9 @@ import at.technikum_wien.tourplanner.viewmodel.TourLogViewModel;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseButton;
 
 public class TourLogController {
     private TourLogViewModel viewModel;
@@ -34,6 +34,33 @@ public class TourLogController {
         durationColumn.setCellValueFactory(new PropertyValueFactory<>("totalTime"));
         commentColumn.setCellValueFactory(new PropertyValueFactory<>("comment"));
         difficultyColumn.setCellValueFactory(new PropertyValueFactory<>("difficulty"));
+
+        //right-click
+        logsTableView.setRowFactory(tv -> {
+            TableRow<TourLog> row = new TableRow<>();
+            ContextMenu contextMenu = new ContextMenu();
+            MenuItem delete = new MenuItem("Delete");
+            MenuItem edit = new MenuItem("Edit");
+            edit.setOnAction((ActionEvent event) -> {
+                TourLog tourLog = row.getItem();
+                System.out.println("edit");
+            });
+
+            delete.setOnAction((ActionEvent event) -> {
+                TourLog tourLog = row.getItem();
+                System.out.println("delete");
+
+            });
+            contextMenu.getItems().addAll(edit, delete);
+            row.setOnMouseClicked(event -> {
+                if (!row.isEmpty() && event.getButton() == MouseButton.SECONDARY) {
+                    contextMenu.show(row, event.getScreenX(), event.getScreenY());
+                }else{
+                    contextMenu.hide();
+                }
+            });
+            return row;
+        });
     }
 
 
