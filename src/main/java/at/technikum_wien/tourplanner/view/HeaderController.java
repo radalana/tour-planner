2b2e98c8b2a78ce6b7f1fc90e52e8d7362426c64
radalana <radalana415@gmail.com>
2025-03-29 11:43:05 +0100

    update header.fxml, HeaderController.java

--- src/main/java/at/technikum_wien/tourplanner/view/HeaderController.java
+++ src/main/java/at/technikum_wien/tourplanner/view/HeaderController.java
@@ -2,9 +2,6 @@ package at.technikum_wien.tourplanner.view;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
@@ -13,26 +10,15 @@ public class HeaderController {
     public TextField searchField;
     @FXML
     private AnchorPane headerPane;
-    @FXML
-    public MenuButton filterMenu;
-    @FXML
-    public CheckBox popularityCheckBox;
-    @FXML
-    public CheckBox childFriendlinessCheckBox;
 
     @FXML
     public void initialize() {
         //initial focus is set away from searchfield
         Platform.runLater(() -> headerPane.requestFocus());
-
-        setupCheckBox(popularityCheckBox);
-        setupCheckBox(childFriendlinessCheckBox);
+    }
+    @FXML
+    private void generateSummaryReport() {
+        // TODO: replace with actual report generation
     }
 
-    private void setupCheckBox(CheckBox checkBox) {
-        checkBox.setOnAction(event -> {
-            event.consume();
-            filterMenu.show();
-        });
-    }
 }
