84ec595bd9bf3a7dc8746cad866bed0e742d0e6a
radalana <radalana415@gmail.com>
2025-06-18 19:09:15 +0200

    refactor: add class TourLogDTO

--- src/main/java/at/technikum_wien/tourplanner/dto/TourLogDTO.java
+++ src/main/java/at/technikum_wien/tourplanner/dto/TourLogDTO.java
@@ -1,4 +1,17 @@
 package at.technikum_wien.tourplanner.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class TourLogDTO {
+    private String dateTime;
+    private String comment;
+    private int difficulty;
+    private double totalDistance;
+    private double totalDuration;
+    private int rating;
 }
