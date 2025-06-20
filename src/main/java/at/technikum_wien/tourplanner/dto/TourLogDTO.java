3df2ca53789ef2a91763f6fe664e65a26a1151dd
radalana <radalana415@gmail.com>
2025-06-20 18:17:28 +0200

    refacator TourLogDTO.java: add id field

--- src/main/java/at/technikum_wien/tourplanner/dto/TourLogDTO.java
+++ src/main/java/at/technikum_wien/tourplanner/dto/TourLogDTO.java
@@ -4,13 +4,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class TourLogDTO {
+    private Long id;
     private String dateTime;
     private String comment;
     private int difficulty;
+    //TODO change to string HH:mm
     private double totalDistance;
     private double totalDuration;
     private int rating;
